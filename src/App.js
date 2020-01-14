e02c3081ce4f852b9270edc214b625d7620ea46a
Zach Pfeifer <mr.zman1@yahoo.com>
2020-01-14 11:53:29 -0700

    Products Rendering

--- src/App.js
+++ src/App.js
@@ -1,26 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Products from "./components/Products";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      products: [],
+      filterProducts: []
+    }
+  }
+
+  componentDidMount() {
+    fetch("http://localhost:8000/products").then(res => res.json())
+      .then(data => this.setState({
+        products: data,
+        filterProducts: data
+      }))
+  }
+
+
+  handleAddToCart = (e, product) => {
+    this.setState(state => {
+      const cartItems = state.cartItems;
+      let productAlreadyInCart = false;
+
+      cartItems.forEach(cp => {
+        if (cp.id === product.id) {
+          cp.count += 1;
+          productAlreadyInCart = true;
+        }
+      });
+
+      if (!productAlreadyInCart) {
+        cartItems.push({ ...product, count: 1 });
+      }
+      localStorage.setItem('cartItems', JSON.stringify(cartItems));
+      return { cartItems: cartItems };
+    });
+  }
+
+  render() {
+
+    return (
+      <div className="conatiner">
+        <h1>Shopping Cart Example</h1>
+        <hr />
+        <div className="row">
+          <div className="col-md-8">
+            <Products products={this.state.filterProducts} handleAddToCart={this.handleAddToCart} />
+          </div>
+          <div className="col-md-4">
+
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
