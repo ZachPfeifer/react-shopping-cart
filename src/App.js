0fcb5d4c2f6c726b8c764076b8d841173420bfa2
Zach Pfeifer <mr.zman1@yahoo.com>
2020-01-14 12:15:12 -0700

    Fixed Super(props)

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ import Basket from './components/Basket';
 import './App.css';
 
 class App extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = { size: '', sort: '', cartItems: [], products: [], filteredProducts: [] };
   }
   componentWillMount() {
